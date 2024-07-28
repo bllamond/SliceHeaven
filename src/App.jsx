da23420c3f50325d0463f994efb75a27d9e97322
bllamond <vinaymalik1729@gmail.com>
2024-07-28 10:25:46 +0530

    priority update

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import CreateOrder ,{action as createOrderAction} from "./features/order/CreateO
 import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 import Error from "./ui/Error";
+import { action as updateOrderAction} from "./features/order/UpdateOrder";
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
@@ -31,6 +32,7 @@ const router = createBrowserRouter([
         element: <Order />,
         loader: orderLoader,
         errorElement: <Error />,
+        action: updateOrderAction,
       },
     ],
   },
